1882e887496fb757524281b9b45b23e0280c2b3e
Waheed Uddin Ahmed <dmc4719@gmail.com>
2023-02-11 16:12:25 +0600

    Fix CustomButton component props type

--- src/components/Common/CustomButton.tsx
+++ src/components/Common/CustomButton.tsx
@@ -1,11 +1,17 @@
-import React from 'react'
+import React from 'react';
 
-type CustomButtonProps = { className? : string} & React.ButtonHTMLAttributes<T>
+type CustomButtonProps = { className?: string } & React.ButtonHTMLAttributes<HTMLButtonElement>;
 
-const CustomButton = ({children, className, ...rest}: CustomButtonProps) => {
-  return (
-    <button type="button" className={`bg-blue-500 text-white rounded-full h-[40px] w-[100px] px-6 ${className}`} {...rest}>{children}</button>
-  )
-}
+const CustomButton = ({ children, className, ...rest }: CustomButtonProps) => {
+    return (
+        <button
+            type='button'
+            className={`bg-blue-500 text-white rounded-full h-[40px] w-[100px] px-6 ${className}`}
+            {...rest}
+        >
+            {children}
+        </button>
+    );
+};
 
-export default CustomButton
+export default CustomButton;
