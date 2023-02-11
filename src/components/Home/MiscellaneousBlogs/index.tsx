36236f2cdf0d8c981d2adfcfdc8edc8f1d3754a1
Waheed Uddin Ahmed <dmc4719@gmail.com>
2023-02-11 16:15:48 +0600

    Fix types

--- src/components/Home/MiscellaneousBlogs/index.tsx
+++ src/components/Home/MiscellaneousBlogs/index.tsx
@@ -2,9 +2,7 @@ import { trendingStories } from '@/data';
 import React from 'react';
 import Card from './Card';
 
-type Props = {};
-
-const index = (props: Props) => {
+const index = () => {
     return (
         <div className='grid grid-cols-3 gap-6 w-full col-span-2 p-6'>
             {trendingStories.map((post) => (
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,7 +4,7 @@ import { DisplayModeProvider } from '@/contexts/DisplayModeContext';
 import type { ReactElement, ReactNode } from 'react';
 import type { NextPage } from 'next';
 
-export type NextPageWithLayout<P = {}, IP = P> = NextPage<P, IP> & {
+export type NextPageWithLayout<P = object, IP = P> = NextPage<P, IP> & {
     getLayout?: (page: ReactElement) => ReactNode;
 };
 
